5919ef2587f645f800871e3c77122755a8de80eb
Vinicius Canuto Menezes <vinicius.canuto@ampm-info.com.br>
2026-02-11 08:16:57 -0300

    :lipstick: feat: Alterações no css da pagina de nota

--- src/app/Model/Note.ts
+++ src/app/Model/Note.ts
@@ -2,6 +2,7 @@ export interface NoteModel {
   id: number;
   userId: number;
   title: string;
+  subtitle?: string;
   content?: string;
   date: string | Date;
   tag: string;
--- src/app/Pages/note/note.ts
+++ src/app/Pages/note/note.ts
@@ -6,15 +6,25 @@ import { DatePipe } from '@angular/common';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { marked } from 'marked';
 import DOMPurify from 'dompurify';
+import { AuthService } from '../../Services/auth.service';
+import { User } from '../../Model/User';
+import { faStar, faGear } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-note',
-  imports: [DatePipe],
+  imports: [DatePipe, FontAwesomeModule],
   templateUrl: './note.html',
   styleUrl: './note.scss',
 })
 
 export class Note implements OnInit {
+
+  iconStar = faStar;
+  iconGear = faGear;
+
+  currentUser: User | null = null;
+
   noteData: NoteModel | null = null;
   renderedContent: SafeHtml | null = null;
 
@@ -22,10 +32,14 @@ export class Note implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private noteService: NoteService,
-    private sanitizer: DomSanitizer
-  ) {}
+    private sanitizer: DomSanitizer,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+
+    this.currentUser = this.authService.getCurrentUser();
+
     this.route.params.subscribe(params => {
       const id = +params['id'];
       this.getNote(id);
@@ -44,4 +58,22 @@ export class Note implements OnInit {
       }
     });
   }
+
+  hasImage(): boolean {
+
+    const content = this.noteData?.content;
+
+    if (!content) return false;
+
+    const mdImageRegex = /!\[.*?\]\((.*?)\)/i;
+    const urlImageRegex = /https?:\/\/\S+\.(png|jpe?g|gif|webp)(\?\S*)?/i;
+    const mdMatch = content.match(mdImageRegex);
+
+    if (mdMatch && mdMatch[1] && urlImageRegex.test(mdMatch[1])) return true;
+
+    if (urlImageRegex.test(content)) return true;
+
+    return false;
+  }
+
 }
